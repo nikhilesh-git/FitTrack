329f80036ec389ff4c715756c299bf9507d4f676
Gowrabathuni Nikhilesh <gnikhilesh.2005@gmail.com>
2025-08-29 11:15:49 +0530

    Delete backend/hashscript.js

--- backend/hashscript.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function generateHash() {
-  const password = 'Nikhilesh@2005'; // choose your password
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-}
-
-generateHash();
